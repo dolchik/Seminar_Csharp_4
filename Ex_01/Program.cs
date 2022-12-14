1070b61950572c421088aec7a7e50c6ddca4002e
OLGA DRACH <dolchik88@mail.ru>
2022-12-14 19:33:37 +0300

    Сумма чмсел от 1 до А

---
+++ Ex_01/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) 
+// и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Clear();
+// получить число от пользователя
+int EntryDigit(string str)
+{
+    Console.Write(str);
+    int numb = int.Parse(Console.ReadLine());
+    return numb;
+}
+// пишем цикл от 1 до А
+// суммируем каждый итератор
+
+int FindSum(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+       sum = sum + i; 
+    }
+    return sum;
+}
+
+int number = EntryDigit("Введите число: ");
+
+int result = FindSum(number);
+Console.WriteLine("Сумма всех чисел до А = " +result);
